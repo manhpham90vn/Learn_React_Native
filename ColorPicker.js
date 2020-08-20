6ee14ea06a19c2a3d5ca9bf2550f27d639670df8
Manh Pham <manhpham90vn@icloud.com>
2020-08-20 23:56:50 +0700

    slider and text input

--- ColorPicker.js
+++ ColorPicker.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet, Platform, Slider, TextInput } from 'react-native';
 
 const ColorPicker = () => {
     return(
         <View style={styles.container}>
+
             <View style={styles.header}>
                 <Text style={styles.headerText}>Color Picker</Text>
             </View>
+
+            <View style={{flex: 1, backgroundColor: 'green', justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{ width: 300, height: 300, backgroundColor: 'white', flexDirection: 'column' }}>
+                    <View style={{ flex: 1, backgroundColor: 'yellow' }}>
+                        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                            <Text>R</Text>
+                            <Slider style={{width: 200, marginLeft: 5, marginRight: 5}} />
+                            <TextInput value="0" style={styles.textInput} />
+                        </View>
+                        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                            <Text>G</Text>
+                            <Slider style={{width: 200, marginLeft: 5, marginRight: 5}} />
+                            <TextInput value="0" style={styles.textInput} />
+                        </View>
+                        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                            <Text>B</Text>
+                            <Slider style={{width: 200, marginLeft: 5, marginRight: 5}} />
+                            <TextInput value="0" style={styles.textInput} />
+                        </View>
+                    </View>
+                    <View style={{ flex: 1, backgroundColor: 'red' }}>
+
+                    </View>
+                </View>
+            </View>
+
         </View>
     )
 };
@@ -42,6 +69,18 @@ const styles = StyleSheet.create({
             }
         })
     },
+    textInput: {
+        width: 50,
+        height: 30,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingBottom: Platform.select({
+            ios: 0,
+            android: 5
+        }),
+        textAlign: 'center'
+    }
 })
 
 export default ColorPicker;
