411faadcd5cc187e121bfb13c71853e38f7a515c
Manh Pham <manhpham90vn@icloud.com>
2020-08-22 11:59:26 +0700

    fix import life cycle

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Lifecycle from './app/Lifecycle';
+import LifecycleMain from './app/Lifecycle';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Lifecycle);
+AppRegistry.registerComponent(appName, () => LifecycleMain);
